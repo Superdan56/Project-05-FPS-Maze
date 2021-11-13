0f7f20bcb2f2b4dfceac08772eca19653be110e9
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2021-11-13 18:51:50 -0500

    menu stuff pt1

--- Global.gd
+++ Global.gd
@@ -1,5 +1,18 @@
 extends Node
 
-func _unhandled_input(_event):
-	if Input.is_action_pressed("menu"):
-		get_tree().quit()
+func _ready():
+	pause_mode = PAUSE_MODE_PROCESS
+
+func _unhandled_input(event):
+	if event.is_action_pressed("menu"):
+		#get_tree().quit()
+		var menu = get_node_or_null("/root/Game/Menu")
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+		if menu != null:
+			if not menu.visible:
+				menu.show()
+				get_tree().paused = true
+			else:
+				menu.hide()
+				get_tree().paused = false
